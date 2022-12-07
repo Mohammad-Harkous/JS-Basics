bdec6b0ff34a53bec5eeb3b41aee9309aea49bfc
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-07 13:14:32 +0200

    Step_6 Completed

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,15 @@
+var number_1 = "";
+
+var number_2 ="";
+
+
+alert("Please enter two number for the division")
+
+number_1 = prompt("Please enter first number");
+
+number_2 = prompt("Please enter second number");
+ 
+var result = number_1 % number_2;
+
+
+alert ("Your result is : " + result);
