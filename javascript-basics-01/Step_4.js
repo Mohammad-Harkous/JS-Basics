ae81b8de229b93eaafcffb14617b3b4ec8ecb498
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-07 12:44:37 +0200

    Step_4 Completed

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,18 @@
+
+var name = "";
+
+var surname = "";
+
+var city = "";
+
+
+
+
+name = prompt("Please enter your Name");
+
+surname = prompt("Please enter your Surname");
+
+city = prompt("Please enter your City");
+
+
+alert("Name: " + name + "\nSurname: " + surname + "\nCity: " + city);
