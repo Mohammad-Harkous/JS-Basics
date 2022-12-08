2a08df7905b62cd28f7339d24c88f46a37ccdc7c
Mohammad-Harkous <mohammad.a.harkous@gmail.com>
2022-12-08 10:18:02 +0200

    Step_2 Completed

---
+++ javascript-basics-03/step 2/Step_2.js
@@ -0,0 +1,12 @@
+function anchor(display) {
+
+    var text = document.getElementById("texte");
+    if (display === "show") {
+        text.style.display = "block";
+
+    } else if (display === "hide") {
+
+        text.style.display = "none";
+    }
+
+}
